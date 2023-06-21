b4ced59664c5a280c3a76f4edbe99ebca54adb98
Russ-chts <sleep9097@gmail.com>
2023-06-21 09:57:13 +0800

    商品功能 - 實作查詢商品列表(基礎實作)

--- springboot-mall/src/main/java/com/russ/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/russ/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.russ.springbootmall.dao;
 import com.russ.springbootmall.dto.ProductRequest;
 import com.russ.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- springboot-mall/src/main/java/com/russ/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/russ/springbootmall/service/ProductService.java
@@ -3,7 +3,12 @@ package com.russ.springbootmall.service;
 import com.russ.springbootmall.dto.ProductRequest;
 import com.russ.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
